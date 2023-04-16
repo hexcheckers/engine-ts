97915b941ad0df5f4cd7730d445bf5ff01da27d4
brevis <brevis.ua@gmail.com>
2023-04-16 23:13:57 +0300

    Fix index.ts

--- index.ts
+++ index.ts
@@ -1,12 +1,7 @@
-// @ts-ignore
-import Ai from "./src/Ai.ts";
-// @ts-ignore
-import Board, {Size} from "./src/Board.ts";
-// @ts-ignore
-import Cell from "./src/Cell.ts";
-// @ts-ignore
-import Move from "./src/Move.ts";
-// @ts-ignore
-import State from "./src/State.ts";
+import Ai from "./src/Ai.js";
+import Board, {Size} from "./src/Board.js";
+import Cell from "./src/Cell.js";
+import Move from "./src/Move.js";
+import State from "./src/State.js";
 
 export {Ai as HcAi, Board as HcBoard, Cell as HcCell, Move as HcMove, Size as HcSize, State as HcState};
