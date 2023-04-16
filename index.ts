f50fcb8acbc7b43b31b3c1c1ec002a7dcf9f8d76
brevis <brevis.ua@gmail.com>
2023-04-16 23:11:29 +0300

    Update configs

---
+++ index.ts
@@ -0,0 +1,12 @@
+// @ts-ignore
+import Ai from "./src/Ai.ts";
+// @ts-ignore
+import Board, {Size} from "./src/Board.ts";
+// @ts-ignore
+import Cell from "./src/Cell.ts";
+// @ts-ignore
+import Move from "./src/Move.ts";
+// @ts-ignore
+import State from "./src/State.ts";
+
+export {Ai as HcAi, Board as HcBoard, Cell as HcCell, Move as HcMove, Size as HcSize, State as HcState};
