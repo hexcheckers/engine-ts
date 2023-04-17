79a8cd95a142c2710f7b7a019d5e419ccade4be9
brevis <brevis.ua@gmail.com>
2023-04-17 20:33:19 +0300

    Update package.json, index.ts

--- index.ts
+++ index.ts
@@ -4,13 +4,4 @@ import Cell from "./src/Cell.js";
 import Move, { Direction } from "./src/Move.js";
 import Piece, { Color } from "./src/Piece.js";
 import State from "./src/State.js";
-
-export const HcAi = Ai;
-export const HcBoard = Board;
-export const HcCell = Cell;
-export const HcColor = Color;
-export const HcDirection = Direction;
-export const HcMove = Move;
-export const HcPiece = Piece;
-export const HcSize = Size;
-export const HcState = State;
+export {Ai, Board, Cell, Color, Direction, Move, Piece, Size, State};
